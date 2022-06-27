fd60d761e83611f5a1826ef54907a4f7359ed0c8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-27 14:20:22 +0900

    feat: add stroke to header logo

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -3,7 +3,11 @@ import { Box, Heading } from '@chakra-ui/react';
 const Header = () => (
   <header>
     <Box w="100%" color="white" p="4" bgColor="teal.400">
-      <Heading as="h1" size="xl">
+      <Heading
+        as="h1"
+        size={{ base: 'lg', xl: 'xl' }}
+        className="text-stroke-blue-700 text-stroke-1"
+      >
         SEICHI KEIBA
       </Heading>
     </Box>

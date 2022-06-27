0206d46b4b7484450ae84ded12a78cc95ca7af73
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-27 15:08:14 +0900

    fix: replace class with type of News

--- src/types/news.ts
+++ src/types/news.ts
@@ -1,34 +1,4 @@
-import * as dateFns from 'date-fns';
-
-class News {
+export type News = {
   title: string;
-
   contents: string[];
-
-  startDate: Date;
-
-  endDate: Date;
-
-  constructor(
-    title: string,
-    contents: string[],
-    startDate: string,
-    endDate: string,
-  ) {
-    // TODO: catch errors
-    const start = dateFns.parse(startDate, 'yyyy-MM-dd', new Date());
-    const end = dateFns.parse(endDate, 'yyyy-MM-dd', new Date());
-
-    if (start >= end) {
-      // TODO: throw error
-      throw Error('start <= end');
-    }
-
-    this.title = title;
-    this.contents = contents;
-    this.startDate = start;
-    this.endDate = end;
-  }
-}
-
-export default News;
+};

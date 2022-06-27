0eb11054fe86ce137da1845a21e223ec34e7152c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-27 22:23:36 +0900

    refactor: MenuItemsをくくりだす

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,65 +1,12 @@
-import { BiCalculator } from 'react-icons/bi';
-import {
-  MdOutlineHowToVote,
-  MdOutlineCreditCard,
-  MdOutlineChat,
-  MdOutlineLock,
-} from 'react-icons/md';
-
-import paths from '@/paths';
+import { admins, playersShow, playersVote } from '@/data/menuItems';
 
 import Home from './presenter';
 
-import type { MenuItem } from '../../common/Menu';
-
-const playersMenuItems: MenuItem[] = [
-  {
-    title: '通常投票',
-    color: 'orange',
-    iconType: MdOutlineHowToVote,
-    to: `${paths.votes.index}/${paths.votes.normal}`,
-  },
-  {
-    title: 'オッズ投票',
-    color: 'orange',
-    iconType: BiCalculator,
-    to: `${paths.votes.index}/${paths.votes.odds}`,
-  },
-  {
-    title: '投票内容照会',
-    color: 'green',
-    iconType: MdOutlineCreditCard,
-    to: `${paths.show.index}/${paths.show.votes}`,
-  },
-  {
-    title: 'レース情報照会',
-    color: 'green',
-    iconType: MdOutlineChat,
-    to: `${paths.show.index}/${paths.show.races}`,
-  },
-  {
-    title: '登録内容修正・退会',
-    color: 'gray',
-    iconType: MdOutlineLock,
-    to: `${paths.show.index}/${paths.show.account}`,
-  },
-];
-
-const adminMenuItems: MenuItem[] = [
-  {
-    title: '投票内容照会',
-    color: 'green',
-    iconType: MdOutlineHowToVote,
-  },
-  {
-    title: 'レース情報登録',
-    color: 'green',
-    iconType: MdOutlineHowToVote,
-  },
-];
-
 const EnhancedHome = () => (
-  <Home playersMenuItems={playersMenuItems} adminMenuItems={adminMenuItems} />
+  <Home
+    playersMenuItems={playersVote.concat(playersShow)}
+    adminMenuItems={admins}
+  />
 );
 
 export default EnhancedHome;
---
+++ src/data/menuItems.ts
@@ -0,0 +1,59 @@
+import { BiCalculator } from 'react-icons/bi';
+import {
+  MdOutlineChat,
+  MdOutlineCreditCard,
+  MdOutlineHowToVote,
+  MdOutlineLock,
+} from 'react-icons/md';
+
+import { MenuItem } from '@/components/common/Menu';
+import paths from '@/paths';
+
+export const playersVote: MenuItem[] = [
+  {
+    title: '通常投票',
+    color: 'orange',
+    iconType: MdOutlineHowToVote,
+    to: `${paths.votes.index}/${paths.votes.normal}`,
+  },
+  {
+    title: 'オッズ投票',
+    color: 'orange',
+    iconType: BiCalculator,
+    to: `${paths.votes.index}/${paths.votes.odds}`,
+  },
+];
+
+export const playersShow: MenuItem[] = [
+  {
+    title: '投票内容照会',
+    color: 'green',
+    iconType: MdOutlineCreditCard,
+    to: `${paths.show.index}/${paths.show.votes}`,
+  },
+  {
+    title: 'レース情報照会',
+    color: 'green',
+    iconType: MdOutlineChat,
+    to: `${paths.show.index}/${paths.show.races}`,
+  },
+  {
+    title: '登録内容修正・退会',
+    color: 'gray',
+    iconType: MdOutlineLock,
+    to: `${paths.show.index}/${paths.show.account}`,
+  },
+];
+
+export const admins: MenuItem[] = [
+  {
+    title: '投票内容照会',
+    color: 'green',
+    iconType: MdOutlineHowToVote,
+  },
+  {
+    title: 'レース情報登録',
+    color: 'green',
+    iconType: MdOutlineHowToVote,
+  },
+];

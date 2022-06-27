56bbd8638b5bb1a483dd50833c161cfaff5b830c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-27 15:25:20 +0900

    feat: add 404 page

--- src/siteRoutes.tsx
+++ src/siteRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+import NotFound from '@/components/pages/404';
 import Home from '@/components/pages/home';
 
 const SiteRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );

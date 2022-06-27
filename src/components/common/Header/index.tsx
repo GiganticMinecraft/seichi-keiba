f11abdd28dde5d782077899e315e191905a5a70e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-27 23:06:01 +0900

    refactor: adjust title in header
    * wrap by button
    * centering in mobile
    * react when hover and focus
    * fix font size

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,25 +1,33 @@
-import { Box, Heading, Link } from '@chakra-ui/react';
+import { Box, Button, Heading, Link } from '@chakra-ui/react';
 import { Link as ReactLink } from 'react-router-dom';
 
 import paths from '@/paths';
 
 const Header = () => (
   <header>
-    <Box w="100%" color="white" p={4} bgColor="teal.200">
-      <Link
-        as={ReactLink}
-        to={paths.home}
-        _hover={{ textDecoration: 'none' }}
-        _focus={{ textDecoration: 'none' }}
-      >
-        <Heading
-          as="h1"
-          size={{ base: 'lg', xl: 'xl' }}
-          className="text-stroke-blue-700 text-stroke-1"
+    <Box
+      w="100%"
+      p={4}
+      bgColor="teal.300"
+      display="flex"
+      justifyContent={{ base: 'center', md: 'start' }}
+    >
+      <Button colorScheme="teal" variant="ghost" textColor="white">
+        <Link
+          as={ReactLink}
+          to={paths.home}
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ textDecoration: 'none' }}
         >
-          SEICHI KEIBA
-        </Heading>
-      </Link>
+          <Heading
+            as="h1"
+            size="xl"
+            className="text-stroke-blue-700 text-stroke-1"
+          >
+            SEICHI KEIBA
+          </Heading>
+        </Link>
+      </Button>
     </Box>
   </header>
 );

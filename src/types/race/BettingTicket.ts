5b515249b09fec7037f0a59b71200eb979a31e05
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-28 17:08:28 +0900

    feat: add BettingTicket model

---
+++ src/types/race/BettingTicket.ts
@@ -0,0 +1,8 @@
+const BettingTicket = {
+  Win: '単勝',
+  Place: '複勝',
+  Exacta: "馬単",
+  Tierce: '3連単',
+} as const;
+
+export default BettingTicket;
--- src/types/race/index.ts
+++ src/types/race/index.ts
@@ -1,7 +1,8 @@
+import BettingTicket from './BettingTicket';
 import { CourseTypes } from './courseType';
 import * as horse from './horse';
 import * as jockey from './jockey';
 import * as odds from './odds';
 import * as race from './race';
 
-export { CourseTypes, horse, jockey, odds, race };
+export { CourseTypes, horse, jockey, odds, race, BettingTicket };

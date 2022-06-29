dc1538565f156927ebfde59366823037e787ffe9
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-29 12:15:05 +0900

    chore: add race data

--- src/components/pages/Votes/Normal/SelectRace/index.tsx
+++ src/components/pages/Votes/Normal/SelectRace/index.tsx
@@ -5,17 +5,24 @@ import SelectRace from './presenter';
 // TODO: receive props or context!
 const data: race.Race[] = [
   {
-    number: race.RaceNumber.of(1),
-    name: race.RaceName.of('race'),
+    number: race.RaceNumber.of(9),
+    name: race.RaceName.of('文月賞'),
     startDateTime: new Date(),
     distance: race.RaceDistance.of(2000),
-    course: CourseTypes.Jump,
+    course: CourseTypes.Dirt,
   },
   {
-    number: race.RaceNumber.of(2),
-    name: race.RaceName.of('race'),
+    number: race.RaceNumber.of(10),
+    name: race.RaceName.of('マインサマーダッシュ'),
     startDateTime: new Date(),
-    distance: race.RaceDistance.of(2000),
+    distance: race.RaceDistance.of(1000),
+    course: CourseTypes.Turf,
+  },
+  {
+    number: race.RaceNumber.of(11),
+    name: race.RaceName.of('整地鯖6周年記念杯'),
+    startDateTime: new Date(),
+    distance: race.RaceDistance.of(1000),
     course: CourseTypes.Jump,
   },
 ];

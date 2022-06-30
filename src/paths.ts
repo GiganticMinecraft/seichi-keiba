83a0a36d88d79c7597c8aa9b7f0cb79cbb842a2d
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-30 11:22:41 +0900

    feat: add paths

---
+++ src/paths.ts
@@ -0,0 +1,21 @@
+const paths = {
+  home: '/',
+  votes: {
+    index: 'votes',
+    normal: 'normal',
+    odds: 'odds',
+  },
+  show: {
+    index: 'show',
+    votes: 'votes',
+    races: 'races',
+    account: 'account',
+  },
+  admins: {
+    index: 'admins',
+    votes: 'votes',
+    races: 'races',
+  },
+};
+
+export default paths;

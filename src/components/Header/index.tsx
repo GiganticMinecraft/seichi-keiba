72cef572411b3ff48cd2df46aabc1b42e5a35c3c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-30 11:38:10 +0900

    feat: add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Heading, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+import paths from '@/paths';
+
+const Header = () => (
+  <header>
+    <Box
+      w="100%"
+      p={4}
+      bgColor="teal.300"
+      display="flex"
+      justifyContent={{ base: 'center', md: 'start' }}
+    >
+      <Button colorScheme="teal" variant="ghost" textColor="white">
+        <Link
+          as={NextLink}
+          href={paths.home}
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ textDecoration: 'none' }}
+        >
+          <Heading
+            as="h1"
+            size="xl"
+            className="text-stroke-blue-700 text-stroke-1"
+          >
+            SEICHI KEIBA
+          </Heading>
+        </Link>
+      </Button>
+    </Box>
+  </header>
+);
+
+export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,23 @@
 import { Container } from '@chakra-ui/react';
 
+import Header from './Header';
+
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <main>
-    <Container
-      maxW={['container.sm', 'container.md', 'container.lg', 'container.xl']}
-      py={4}
-    >
-      {children}
-    </Container>
-  </main>
+  <>
+    <Header />
+    <main>
+      <Container
+        maxW={['container.sm', 'container.md', 'container.lg', 'container.xl']}
+        py={4}
+      >
+        {children}
+      </Container>
+    </main>
+  </>
 );
 
 export default Layout;

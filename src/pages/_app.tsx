e11fdc91ce5187364050bc31c4bb8aa88f75cbde
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-30 11:06:46 +0900

    chore: init chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
 import type { AppProps } from 'next/app';
+
 // eslint-disable-next-line import/no-unresolved
 import 'windi.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
 export default MyApp;

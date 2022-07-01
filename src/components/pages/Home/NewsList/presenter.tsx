16f5777865d1722987c41325d5692c326aa82223
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-01 12:02:53 +0900

    feat: add NewsList

---
+++ src/components/pages/Home/NewsList/presenter.tsx
@@ -0,0 +1,53 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Box,
+  Text,
+  Alert,
+  AlertIcon,
+} from '@chakra-ui/react';
+
+import type { News } from '@/types/news';
+
+type Props = {
+  newsList?: News[];
+};
+
+const NewsList: React.FCX<Props> = ({
+  newsList = [],
+  className = undefined,
+}) => (
+  <div className={className}>
+    {newsList.length ? (
+      <Accordion defaultIndex={[0]} allowMultiple>
+        {newsList.map((news) => (
+          <AccordionItem key={news.title}>
+            <h3>
+              <AccordionButton>
+                <Box flex={1} textAlign="left">
+                  {news.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h3>
+            <AccordionPanel pb={4}>
+              {news.contents.map((str) => (
+                <Text key={str}>{str}</Text>
+              ))}
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    ) : (
+      <Alert status="info">
+        <AlertIcon />
+        お知らせはありません。
+      </Alert>
+    )}
+  </div>
+);
+
+export default NewsList;

6ae453da19517c0611a4f3cc3e4251f90b1e991f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-01 12:09:05 +0900

    fix: replace admin -> admins

--- src/pages/admin/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-import AdminComponent from '@/components/pages/Admin';
-
-const Admin: NextPage = () => <AdminComponent />;
-
-export default Admin;
---
+++ src/pages/admins/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+import AdminComponent from '@/components/pages/Admins';
+
+const Admins: NextPage = () => <AdminComponent />;
+
+export default Admins;

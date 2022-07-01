294485f0b670d275a0b19f689d82d103b0a506c9
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-01 11:55:22 +0900

    feat: add Menu

---
+++ src/components/common/Menu/index.tsx
@@ -0,0 +1,50 @@
+import { Button, Grid, Icon, GridItem, Text, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { IconType } from 'react-icons';
+
+import paths from '@/data/paths';
+
+export type MenuItem = {
+  title: string;
+  color: string;
+  iconType: IconType;
+  to?: string;
+};
+
+type Props = {
+  items: MenuItem[];
+};
+
+const Menu: React.FCX<Props> = ({ items: menuItems, className }) => (
+  <Grid
+    gap={4}
+    templateColumns={{ md: 'repeat(2, 1fr)' }}
+    className={className}
+  >
+    {menuItems.map((item) => (
+      <GridItem rowSpan={2} key={item.title}>
+        <Link
+          as={NextLink}
+          href={item.to ?? paths.home}
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ textDecoration: 'none' }}
+        >
+          <Button
+            flexDirection="column"
+            bgColor={`${item.color}.300`}
+            w="full"
+            h="full"
+            py={2}
+            _hover={{ backgroundColor: `${item.color}.100` }}
+            _focus={{ backgroundColor: `${item.color}.500` }}
+          >
+            <Icon as={item.iconType} w={10} h={10} mb={1} />
+            <Text fontSize="lg">{item.title}</Text>
+          </Button>
+        </Link>
+      </GridItem>
+    ))}
+  </Grid>
+);
+
+export default Menu;

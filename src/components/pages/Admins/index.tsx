118c9792e1dd77551d51ed1a121b849b803fb731
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-01 14:12:32 +0900

    fix: fix paths from each menu

--- src/components/pages/Admins/index.tsx
+++ src/components/pages/Admins/index.tsx
@@ -1,23 +1,12 @@
-import { MenuItem } from '@/components/common/Menu';
 import H2 from '@/components/common/headings/H2';
 import { admins } from '@/data/menuItems';
-import paths from '@/data/paths';
 
 import Admins from './presenter';
-
-const items: MenuItem[] = admins.map((item) => {
-  const newItem: MenuItem = {
-    ...item,
-    to: item.to?.replace(`${paths.admins.index}/`, ''),
-  };
-
-  return newItem;
-});
 
 const EnhancedAdmins = () => (
   <>
     <H2>管理者メニュー</H2>
-    <Admins items={items} />
+    <Admins items={admins} />
   </>
 );
 
--- src/components/pages/Votes/index.tsx
+++ src/components/pages/Votes/index.tsx
@@ -1,23 +1,12 @@
-import { MenuItem } from '@/components/common/Menu';
 import H2 from '@/components/common/headings/H2';
 import { playersVote } from '@/data/menuItems';
-import paths from '@/data/paths';
 
 import Votes from './presenter';
-
-const items: MenuItem[] = playersVote.map((item) => {
-  const newItem: MenuItem = {
-    ...item,
-    to: item.to?.replace(`${paths.votes.index}/`, ''),
-  };
-
-  return newItem;
-});
 
 const EnhancedVotes = () => (
   <>
     <H2>投票メニュー</H2>
-    <Votes items={items} />
+    <Votes items={playersVote} />
   </>
 );
 

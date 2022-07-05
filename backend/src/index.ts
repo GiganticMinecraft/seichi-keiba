7d69bd28ffa702a58eabf5fce6f234b8aa501359
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-06 00:00:38 +0900

    chore(backend): add ESLint & prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,36 @@
-import { ApolloServer } from "apollo-server";
-import { readFileSync } from "fs";
-import { join } from "path";
-
 import {
   serverSchema,
   asDateString,
-} from "@GiganticMinecraft/seichi-keiba-shared";
+} from '@gigantic-minecraft/seichi-keiba-shared';
+import { ApolloServer } from 'apollo-server';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-const path = join(process.cwd(), "../shared/src/apollo", "schema.graphql");
-const typeDefs = readFileSync(path).toString("utf-8");
+const path = join(process.cwd(), '../shared/src/apollo', 'schema.graphql');
+const typeDefs = readFileSync(path).toString('utf-8');
 
 // スキーマと実際のデータ構造の紐付けを resolvers で行う
 const news = [
   {
-    id: "1",
-    title: "title undefined",
-    contents: ["contents"],
-    created_at: asDateString("2022/07/01"),
+    id: '1',
+    title: 'title undefined',
+    contents: ['contents'],
+    created_at: asDateString('2022/07/01'),
     closed_at: undefined,
   },
   {
-    id: "2",
-    title: "title valid",
-    contents: ["contents"],
-    created_at: asDateString("2022/07/01"),
-    closed_at: asDateString("2022-07-10"),
+    id: '2',
+    title: 'title valid',
+    contents: ['contents'],
+    created_at: asDateString('2022/07/01'),
+    closed_at: asDateString('2022-07-10'),
   },
   {
-    id: "3",
-    title: "title invalid",
-    contents: ["contents"],
-    created_at: asDateString("2022/07/01"),
-    closed_at: asDateString("2022-07-01"),
+    id: '3',
+    title: 'title invalid',
+    contents: ['contents'],
+    created_at: asDateString('2022/07/01'),
+    closed_at: asDateString('2022-07-01'),
   },
 ];
 
@@ -40,16 +39,19 @@ const resolvers: serverSchema.Resolvers = {
     news: () => news,
     valid_news: () =>
       news.filter(
-        (value) => !value.closed_at || new Date(value.closed_at) >= new Date()
+        (value) => !value.closed_at || new Date(value.closed_at) >= new Date(),
       ),
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  })
+  .catch((err) => {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`Error has occured!: ${err}`);
+  });

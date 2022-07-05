41573aa94e59d58e841a082b4a34889b2b58262d
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-06 00:09:04 +0900

    chore(backend): add ESLint & prettier

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,6 @@
-import * as serverSchema from "./apollo/generated/server";
-import * as clientSchema from "./apollo/generated/client";
+import * as clientSchema from './apollo/generated/client';
+import * as serverSchema from './apollo/generated/server';
 
 export { serverSchema, clientSchema };
 
-export * from "./types/nonimal";
+export * from './types/nonimal';

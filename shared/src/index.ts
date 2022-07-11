3819a348e725be1fd1ceaf6d3f9591e9f1e10925
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-11 20:39:36 +0900

    fix: graphql-codegenで生成をするファイルを各ワークスペースに配置
    sharedパッケージを介すると、型が一部効かず、anyになってしまう

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,1 @@
-import * as clientSchema from './apollo/generated/client';
-import * as serverSchema from './apollo/generated/server';
-
-export { serverSchema, clientSchema };
-
 export * from './types';

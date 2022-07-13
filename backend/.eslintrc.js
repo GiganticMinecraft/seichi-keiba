2ca5949ec2b42145a23a7f2f161ede9b725a1d45
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-13 15:27:52 +0900

    chore(backend): ignore some rules of ESLint in resolvers

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -105,4 +105,15 @@ module.exports = {
       typescript: [],
     },
   },
+  overrides: [
+    {
+      files: ['./src/resolvers/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+      },
+    },
+  ],
 };

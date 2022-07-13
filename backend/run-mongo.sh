eaebebfac25633ee6dbd78695ca3f8c1d4d7bc06
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-13 12:38:45 +0900

    chore(backend): save init command of mongo

--- backend/run-mongo.sh
+++ backend/run-mongo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd docker
+# ReplicaSetを適用するコマンド。最初に一度実行しないと、DBにアクセスできない
+# docker-compose exec mongodb-primary mongo admin -u admin -p admin /docker-entrypoint-initdb.d/init.js
 docker-compose down
 docker-compose up --remove-orphans
